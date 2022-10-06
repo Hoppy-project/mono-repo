99fa202fc38b1f9b5fedcf624bc92da3fb6be57f
Yoona-J <yoonalove77@naver.com>
2022-10-06 11:05:04 +0900

    [hotfix] fix login request url when is not local

--- frontend/client/src/components/views/LoginPage/KakaoLogin/KakaoLoginPage.js
+++ frontend/client/src/components/views/LoginPage/KakaoLogin/KakaoLoginPage.js
@@ -4,14 +4,28 @@ import Logo from './img/Logo.png'
 import kakaoLoginButton from './img/kakaoLoginButton.png'
 
 function KakaoLoginPage() {
+
+    const loginhandler = () => {
+        if (window.location.href === 'http://localhost:8888/login') {
+            return <>
+                <a href={KAKAO_AUTH_URL}>
+                    <img src={kakaoLoginButton} alt='img' style={{height: '50%'}}/>
+                </a>
+            </>
+        } else if (window.location.href === 'https://hoppy.kro.kr/login') {
+            return <>
+                <a href={KAKAO_AUTH_URL}>
+                    <img src={kakaoLoginButton} alt='img' style={{height: '50%'}}/>
+                </a>
+            </>
+        }
+    }
     return (
         <div style={{textAlign: 'center', marginTop: '180px'}}>
             <img src={Logo} alt='img' style={{width: '217px', height: '109px'}} />
             <h5 style={{fontSize: '11px'}}>지금 바로 Hoppy의 서비스를 시작해보세요!</h5>
             <br />
-            <a href={KAKAO_AUTH_URL}>
-                <img src={kakaoLoginButton} alt='img' style={{height: '50%'}}/>
-            </a>
+            {loginhandler()}
         </div>
     )
 }
