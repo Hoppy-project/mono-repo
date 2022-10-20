61208ca86c29f472f1e635c9605fdfb91ebc451c
Daehan Choi <mnnm22@naver.com>
2022-10-20 17:22:28 +0900

    [fix] 나의 모임 Dto 추가

---
+++ backend/src/main/java/com/hoppy/app/member/dto/LikeMeetingsDto.java
@@ -0,0 +1,30 @@
+package com.hoppy.app.member.dto;
+
+import com.hoppy.app.like.domain.MemberMeetingLike;
+import com.hoppy.app.meeting.domain.Meeting;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LikeMeetingsDto {
+
+    private Long id;
+
+    private String title;
+
+    private String url;
+
+    public static LikeMeetingsDto meetingToDto(MemberMeetingLike meeting) {
+        return LikeMeetingsDto.builder()
+                .id(meeting.getMeetingId())
+                .title(meeting.getMeeting().getTitle())
+                .url(meeting.getMeeting().getUrl())
+                .build();
+    }
+}
